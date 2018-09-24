991c0c6a7e354ba047cdf6817e056a53b31dbd4f
Mohsin Adeel <mohsin.adeel@yahoo.com>
2018-09-24 20:34:05 +0500

    composer autoload psr-4 to psr-0

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -3,7 +3,6 @@
 
 namespace tests;
 
-use mohsinadeel\wpcoreintegrity\core\WP_Core_Integrity;
 use PHPUnit\Framework\TestCase;
 
 class BasicTest extends TestCase
@@ -11,7 +10,7 @@ class BasicTest extends TestCase
 
     public function test_plugin_initiated()
     {
-        $plugin = new WP_Core_Integrity();
+        $plugin = new \mohsinadeel\wpcoreintegrity\core\WP_Core_Integrity();
         $this->assertFalse($plugin->get_initiated());
     }
 }
