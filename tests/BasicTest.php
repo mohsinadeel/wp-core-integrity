b89beb29e61d8dd2ab83c6c30c3f86602280a79f
Mohsin Adeel <mohsin.adeel@yahoo.com>
2018-09-25 00:34:33 +0500

    composer dump-autoload added

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -2,7 +2,7 @@
 
 namespace tests;
 
-//use Inceptionsol\Coreintegrity\core\WP_Core_Integrity;
+use Inceptionsol\Coreintegrity\core\WP_Core_Integrity;
 use PHPUnit\Framework\TestCase;
 
 class BasicTest extends TestCase
@@ -10,8 +10,8 @@ class BasicTest extends TestCase
 
     public function test_plugin_initiated()
     {
-//        $plugin = new WP_Core_Integrity();
-//        $this->assertFalse($plugin->get_initiated());
-        $this->assertTrue(true);
+        $plugin = new WP_Core_Integrity();
+        $this->assertFalse($plugin->get_initiated());
+//        $this->assertTrue(true);
     }
 }
